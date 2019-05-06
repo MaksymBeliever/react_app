db08880f014b3bf3752670bbf2f7bb4b926182d9
Maksym <wolfsangel009@gmail.com>
2019-05-06 19:55:20 +0300

    created app tructure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const Footer = () => {
+   return (
+      <footer className="container">
+         <a href="https://www.linkedin.com/in/maksym-beliba-a7b514181">Web developer Maksym Beliba 2019</a>
+      </footer>
+   )
+}
+
+export default Footer;
---
+++ src/system/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+import gallery from '../components/pages/Gallery/reducer';
+import header from '../components/Header/reducer';
+
+
+export default combineReducers({
+   gallery,
+   header,
+});
